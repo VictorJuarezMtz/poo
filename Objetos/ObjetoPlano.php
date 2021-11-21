216f77f2e1ec43cba76a15375f40f49d5fd8659b
Victor Juarez <developer.victor@blackdish.mx>
2021-11-20 22:59:36 -0600

    master

---
+++ Objetos/ObjetoPlano.php
@@ -0,0 +1,23 @@
+<?php
+#creacion de la clase clientepo
+class ClientePO {
+    #creacion de campos
+    private $nombre;
+    private $apellidos;
+    private $edad;
+
+
+        public function GetNombre(){
+            return $this->nombre;
+        }
+
+        public function SetNombre($nombre){
+            $this->nombre=$nombre;
+        }
+
+
+
+
+}
+
+?>
